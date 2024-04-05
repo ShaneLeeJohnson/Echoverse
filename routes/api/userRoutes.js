0faa19de34ccea5d44e791c1d52894f170fc85d2
shanejohnson <shane.john316@gmail.com>
2024-04-05 18:53:28 -0500

    Updated userRoutes.js by adding put, post and delete routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -29,4 +29,42 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:userId', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId,
+            req.body,
+            { new: true }
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:userId', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.userId);
+        if (!deletedUser) {
+            return res.status(404).json({message: 'User not found!'});
+        }
+        await Thought.deleteMany({ username: deletedUser.username});
+        res.json({message: 'User and associated thoughts deleted!'});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
